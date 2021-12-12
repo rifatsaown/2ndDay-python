f096db37cd62f7df8b00a872ef48bf744ebf8d0c
rifatsaown <rifatsaown0@gmail.com>
2021-12-12 12:27:11 +0600

    variableType added

---
+++ variableType.py
@@ -0,0 +1,11 @@
+import types
+
+
+nameCharacter =len("Rifat Hossen Saown")
+print(type(nameCharacter))
+stringConvert =str(nameCharacter)
+print(type(stringConvert))
+intConvert =int(stringConvert)
+print(type(intConvert))
+
+print("Your name have "+stringConvert+" character")
